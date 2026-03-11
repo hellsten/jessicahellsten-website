468a67080aaf27795ba21cca8886e9746bb214dc
hellsten <jessicahellsten@live.com>
2026-03-11 12:52:41 -0700

    Changed screenshot and Added Netlify rule documents

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react()],
+  build: {
+    outDir: "dist", // default, Netlify expects this
+    emptyOutDir: true,
+  },
 });
