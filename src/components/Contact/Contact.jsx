ff13c3990b698b931e5e28f71f9a215b8413b754
hellsten <jessicahellsten@live.com>
2026-03-13 15:39:44 -0700

    Added form submission in Contact through Formspree

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import { useForm, ValidationError } from "@formspree/react";
 
 import "./Contact.scss";
 
@@ -7,6 +8,55 @@ import LinkedIn from "./../../assets/icons/linkedin.svg?react";
 import Mail from "./../../assets/icons/mail.svg?react";
 
 function Contact() {
+  //from Formspree.io
+  const [state, handleSubmit] = useForm("xlgplnpb");
+  if (state.succeeded) {
+    return (
+        
+    <section className="contact">
+      <div className="contact__container" id="sectionContact">
+        <div className="contact__header">
+          <div className="contact__title">Contact</div>
+        </div>
+        <div className="contact__card">
+          <div className="contact__card-other">
+            <div className="contact__card-find contact__card-find-email">
+              <NavLink
+                className="contact__card-link"
+                to="mailto:jessicahellsten@gmail.com"
+              >
+                <Mail />
+              </NavLink>
+              <div className="contact__card-find-text">Email</div>
+            </div>
+            <div className="contact__card-find contact__card-find-linkedin">
+              <NavLink
+                className="contact__card-link"
+                to="https://www.linkedin.com/in/jessicahellsten/"
+              >
+                <LinkedIn />
+              </NavLink>
+              <div className="contact__card-find-text">LinkedIn</div>
+            </div>
+            <div className="contact__card-find contact__card-find-github">
+              <NavLink
+                className="contact__card-link"
+                to="https://github.com/hellsten"
+              >
+                <GitHub />
+              </NavLink>
+              <div className="contact__card-find-text">GitHub</div>
+            </div>
+          </div>
+          <form onSubmit={handleSubmit} className="contact__card-form">
+            <div className="contact__card-sent">Message Sent</div>
+          </form>
+        </div>
+      </div>
+    </section>
+    );
+  }
+
   return (
     <section className="contact">
       <div className="contact__container" id="sectionContact">
@@ -22,7 +72,7 @@ function Contact() {
               >
                 <Mail />
               </NavLink>
-                <div className="contact__card-find-text">Email</div>
+              <div className="contact__card-find-text">Email</div>
             </div>
             <div className="contact__card-find contact__card-find-linkedin">
               <NavLink
@@ -31,7 +81,7 @@ function Contact() {
               >
                 <LinkedIn />
               </NavLink>
-                <div className="contact__card-find-text">LinkedIn</div>
+              <div className="contact__card-find-text">LinkedIn</div>
             </div>
             <div className="contact__card-find contact__card-find-github">
               <NavLink
@@ -40,39 +90,55 @@ function Contact() {
               >
                 <GitHub />
               </NavLink>
-                <div className="contact__card-find-text">GitHub</div>
+              <div className="contact__card-find-text">GitHub</div>
             </div>
           </div>
-          <div className="contact__card-form">
-            <label>Name</label>
+          <form onSubmit={handleSubmit} className="contact__card-form">
+            <label htmlFor="name">Name</label>
             <input
               type="text"
               id="name"
+              name="name"
               className="contact__card-name"
               placeholder="Your Name"
             />
-            <label>Email</label>
+            <label htmlFor="email">Email</label>
             <input
-              type="text"
+              type="email"
               id="email"
+              name="email"
               className="contact__card-email"
               placeholder="Your@email.com"
             />
-            <label>Subject</label>
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+            <label htmlFor="subject">Subject</label>
             <input
               type="text"
               id="subject"
+              name="subject"
               className="contact__card-subject"
               placeholder="Subject"
             />
-            <label>Message</label>
+            <label htmlFor="message">Message</label>
             <textarea
               id="message"
+              name="message"
               className="contact__card-message"
               placeholder="Your Message"
             ></textarea>
-            <div className="contact__card-form-submit">Submit</div>
-          </div>
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
+            <button type="submit" className="contact__card-form-submit">
+              Submit
+            </button>
+          </form>
         </div>
       </div>
     </section>
