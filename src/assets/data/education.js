a856e593281f3093c87e93cc451d25e25b3fd560
hellsten <jessicahellsten@live.com>
2026-03-13 10:26:54 -0700

    Added Education school logos and styling

---
+++ src/assets/data/education.js
@@ -0,0 +1,25 @@
+import Uvic from "./../images/uvic.svg"
+import BrainStation from "./../images/brainstationwhite.png"
+
+const education = [
+    {
+        id: "brainstation",
+        school: "BrainStation",
+        degree: "Diploma",
+        major: "Software Engineering",
+        dates: "August 2025 - March 2026",
+        location: " ",
+        logo: BrainStation,
+    },
+    {
+        id: "uVic",
+        school: "University of Victoria",
+        degree: "Bachelor of Applied Science",
+        major: "Biomedical Engineering",
+        dates: "September 2016 - May 2021",
+        location: "Victoria, BC",
+        logo: Uvic,
+    },
+]
+
+export default education;
--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,5 +1,5 @@
 import "./Education.scss";
-//import education from "./../../assets/data/education.js";
+import education from "./../../assets/data/education.js";
 
 function Education() {
   return (
@@ -8,7 +8,19 @@ function Education() {
         <div className="education__header">
           <div className="education__title">Education</div>
         </div>
-        <div className="education__card-container">
+        <div className="education__card-outside">
+            <div className="education__card-container">
+            {education.map((education) => (
+            <div key={education.id} className="education__card">
+              <div className="education__card-details">
+                <div className="education__card-dates">{education.dates}</div>
+                <div className="education__card-degree">{education.degree}</div>
+                <div className="education__card-major">{education.major}</div>
+              </div>
+              <img className="education__card-logo" src={education.logo}/>
+            </div>
+          ))}
+        </div>
         </div>
       </div>
     </section>
